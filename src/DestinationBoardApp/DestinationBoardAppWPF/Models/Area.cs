7889fd359b04854f3aa018b6d1763d7d6767cbe1
Hibiki Fujimori <hibikifujimori@live.jp>
2020-03-24 22:48:53 +0900

    各種クラスの追加

---
+++ src/DestinationBoardApp/DestinationBoardAppWPF/Models/Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace DestinationBoardAppWPF
+{
+    class Area
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Boolean IsTop { get; set; }
+        public Boolean IsEnd { get; set; }
+        public int ParentId { get; set; }
+    }
+}
