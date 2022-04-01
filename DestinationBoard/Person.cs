047c09fab4f299dd2f1763f8419f00f5a848c9b5
K-Nishikawa-Ushisan-Mo-Mo <k.n.kgte-705@outlook.jp>
2022-04-01 22:36:46 +0900

    Add Person Class

---
+++ DestinationBoard/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DestinationBoard
+{
+    public class Person
+    {
+        /// <summary>
+        /// 人の名前
+        /// </summary>
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// 所属組織
+        /// </summary>
+        public string Organization
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
