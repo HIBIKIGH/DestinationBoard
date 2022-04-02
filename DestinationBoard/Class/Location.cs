b0b3d7fdc0190a648676f401bcf4d30f1b42c2bf
HIBIKI <hibikifujimori@gmail.com>
2022-04-02 14:37:11 +0900

    Locationクラスの追加

---
+++ DestinationBoard/Class/Location.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DestinationBoard
+{
+    public class Location
+    {
+        /// <summary>
+        /// 行先の名称
+        /// </summary>
+        public String Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// 行先の親
+        /// </summary>
+        public Location ParentLocation
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
